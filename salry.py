928f134d36948e564b455668f03009afd388a053
bantylahri95-oss <bantylahri95@gmail.com>
2026-02-18 12:08:23 +0530

    first commit

---
+++ salry.py
@@ -0,0 +1,12 @@
+# Given values
+basic_salary = 25000
+hra_percentage = 20
+bonus = 5000
+
+# Calculations
+hra = (basic_salary * hra_percentage) / 100
+total_salary = basic_salary + hra + bonus
+
+# Output
+print("HRA: ₹", hra)
+print("Total Monthly Salary: ₹", total_salary)
