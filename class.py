e6ab1c42cabe8018822f71b964bb332f463766a3
bantylahri95-oss <bantylahri95@gmail.com>
2026-02-18 12:25:47 +0530

    first commit

---
+++ class.py
@@ -0,0 +1,11 @@
+# Given values
+total_students = 125
+students_per_class = 30
+
+# Calculations
+full_classes = total_students // students_per_class
+remaining_students = total_students % students_per_class
+
+# Output
+print("Full Classes:", full_classes)
+print("Remaining Students:", remaining_students)
