e2a3fa917f7afa9e89710f7ca6cc1c3dc66bc8ce
bantylahri95-oss <bantylahri95@gmail.com>
2026-02-18 23:14:58 +0530

    first commit

---
+++ profit_los.py
@@ -0,0 +1,13 @@
+# Program to calculate the perimeter of a triangle
+
+# Taking input from user
+side_one = float(input("Enter the length of first side: "))
+side_two = float(input("Enter the length of second side: "))
+side_three = float(input("Enter the length of third side: "))
+
+# Calculating perimeter
+triangle_perimeter = side_one + side_two + side_three
+
+# Displaying result
+print("\n----- Result -----")
+print("Perimeter of the triangle is:", triangle_perimeter, "cm")
