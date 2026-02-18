327dfd9c866ff0c805a7bcfd54925bca02c5e4f9
bantylahri95-oss <bantylahri95@gmail.com>
2026-02-18 23:41:05 +0530

    profit or loss

--- profit_los.py
+++ profit_los.py
@@ -1,13 +1,23 @@
-# Program to calculate the perimeter of a triangle
-
-# Taking input from user
-side_one = float(input("Enter the length of first side: "))
-side_two = float(input("Enter the length of second side: "))
-side_three = float(input("Enter the length of third side: "))
-
-# Calculating perimeter
-triangle_perimeter = side_one + side_two + side_three
-
-# Displaying result
-print("\n----- Result -----")
-print("Perimeter of the triangle is:", triangle_perimeter, "cm")
+#program to calculate profit or loss based on cost price and selling price
+#input of cost price 
+cp=float(input("Enter the cost price:"))
+#to validate the cost price
+if(cp<=0):
+    print("Invalid cost price")
+    exit()
+    #exiting the program if cost price is invalid
+else:#input of selling price
+    sp=float(input("Enter the selling price:"))
+    #to validate the selling price 
+    if(sp<=0):
+        print("Invalid selling pricce")
+        exit()
+        #exiting the program if sellling price is invalid
+    else:
+        #calculating profit or loss
+        if(sp>cp):
+        print("profit:",sp-cp)
+        elseif(cp>sp):
+        print("loss:",cpp-sp)
+    else:
+        print("No profit no  Loss")
